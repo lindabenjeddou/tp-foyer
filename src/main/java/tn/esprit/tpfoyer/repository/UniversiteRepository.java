49670afe8e8289f173cc499a87cbdd33d7fc87f3
ikbel.benmrad <medikbel09@gmail.com>
2024-10-30 19:42:21 +0100

    First Commit

---
+++ src/main/java/tn/esprit/tpfoyer/repository/UniversiteRepository.java
@@ -0,0 +1,44 @@
+/*package tn.esprit.tpfoyer.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tn.esprit.tpfoyer.entity.Universite;
+
+import java.util.List;
+
+@Repository
+public interface UniversiteRepository extends JpaRepository<Universite, Long>
+{
+
+
+    List<Universite> searchUniversites(String nomUniversite, String addresse);
+
+
+    void deleteAllUniversities();
+}*/
+package tn.esprit.tpfoyer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import tn.esprit.tpfoyer.entity.Universite;
+
+import java.util.List;
+
+@Repository
+public interface UniversiteRepository extends JpaRepository<Universite, Long> {
+
+    // Custom search method with partial matching on nomUniversite and adresse
+    @Query("SELECT u FROM Universite u WHERE u.nomUniversite LIKE %:nomUniversite% AND u.adresse LIKE %:adresse%")
+    List<Universite> searchUniversites(@Param("nomUniversite") String nomUniversite, @Param("adresse") String adresse);
+
+    // Custom delete method to remove all universities
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Universite")
+    void deleteAllUniversities();
+}
---
+++ src/main/java/tn/esprit/tpfoyer/service/IUniversiteService.java
@@ -0,0 +1,24 @@
+package tn.esprit.tpfoyer.service;
+
+import tn.esprit.tpfoyer.entity.Universite;
+
+import java.util.List;
+
+public interface IUniversiteService {
+
+    List<Universite> retrieveAllUniversites();
+    Universite retrieveUniversite(Long universiteId);
+    Universite addUniversite(Universite f);
+    void removeUniversite(Long universiteId);
+    Universite modifyUniversite(Universite universite);
+
+    // Here we will add later methods calling keywords and methods calling JPQL
+
+    List<Universite> searchUniversites(String nomUniversite, String addresse);
+
+    List<Universite> bulkUpdateUniversites(List<Universite> universites);
+
+    void deleteAllUniversities();
+
+
+}
