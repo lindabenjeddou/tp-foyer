126667391f01f18ec83091944d468ad7f4d9d1a0
Mahasallemi <117112305+Mahasallemi@users.noreply.github.com>
2024-11-06 13:43:29 +0100

    unit

--- src/test/java/EtudiantServiceImplTest.java
+++ src/test/java/EtudiantServiceImplTest.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-public class EtudiantServiceImplTest {
+ class EtudiantServiceImplTest {
     @Mock
     private EtudiantRepository etudiantRepository;
 
